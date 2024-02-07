c6ad83019a24570117a63b7dca4de8114e1967ca
Aryan Arora <82714299+Aryan1612@users.noreply.github.com>
2024-02-07 18:58:23 +0530

    Update lax_2.m

--- lax_2.m
+++ lax_2.m
@@ -1,39 +1,56 @@
-M = zeros(38, 201);
-delta_x = 0.01;
-delta_t = 0.8*0.01;
 lambda = 0.8;
-x_data_1 = linspace(-1, 1, 201);
-x_data_2 = linspace(-1, 1, 5001);
-for i = 1:201
-    if (abs(2*i/200 - 51/50) < (1/3))
+n_x = 200;
+dx = 2/n_x;
+dt = lambda*dx;
+n_t = round(0.3/dt);
+
+M = zeros(n_t+1, n_x+1);
+
+for i = 1:n_x+1
+    if (abs(-1 + dx*(i-1)) < (1/3))
         M(1, i) = 1;
     else
         M(1, i) = -1;
     end
 end
 
-for row = 2:38
-    for col = 2:200
-        M(row, col) = 0.5*(M(row-1, col+1) + M(row-1, col-1))*(1-0.4*0.5*(M(row-1, col+1)-M(row-1, col-1)));
+for i = 2:n_t+1
+    for j = 2:n_x
+        M(i, j) = 0.5*(M(i-1, j+1) + M(i-1, j-1)) - lambda*0.5*(0.5*(M(i-1, j+1))*(M(i-1, j+1)) - 0.5*(M(i-1, j-1))*(M(i-1, j-1)));
     end
-    M(row, 1) = 0.5*(M(row-1, 200) + M(row-1, 2))*(1-0.4*0.5*(M(row-1, 2)-M(row-1, 200)));
-    M(row, 201) = M(row, 1);
+    M(i, 1) = 0.5*(M(i-1, n_x) + M(i-1, 2)) - lambda*0.5*0.5*((M(i-1, 2))^2 - (M(i-1, n_x))^2);
+    M(i, n_x+1) = M(i, 1);
 end
-M
-N = zeros(38, 5001);
-for i = 1:5001
-    if (abs(2*i/5000 - 2501/2500) < (1/3))
-        N(1, i) = 1;
+
+x_data_1 = linspace(-1, 1, n_x+1);
+y_data_1 = M(n_t+1, :);
+
+lambda = 0.8;
+n_x = 5000;
+dx = 2/n_x;
+dt = lambda*dx;
+n_t = round(0.3/dt);
+
+M = zeros(n_t+1, n_x+1);
+
+for i = 1:n_x+1
+    if (abs(-1 + dx*(i-1)) < (1/3))
+        M(1, i) = 1;
     else
-        N(1, i) = -1;
+        M(1, i) = -1;
     end
 end
 
-for row = 2:38
-    for col = 2:5000
-        N(row, col) = 0.5*(N(row-1, col+1) + N(row-1, col-1))*(1-0.4*0.5*(N(row-1, col+1)-N(row-1, col-1)));
+for i = 2:n_t+1
+    for j = 2:n_x
+        M(i, j) = 0.5*(M(i-1, j+1) + M(i-1, j-1)) - lambda*0.5*(0.5*(M(i-1, j+1))*(M(i-1, j+1)) - 0.5*(M(i-1, j-1))*(M(i-1, j-1)));
     end
-    N(row, 1) = 0.5*(N(row-1, 5000) + N(row-1, 2))*(1-0.4*0.5*(N(row-1, 2)-N(row-1, 5000)));
-    N(row, 5001) = N(row, 1);
+    M(i, 1) = 0.5*(M(i-1, n_x) + M(i-1, 2)) - lambda*0.5*0.5*((M(i-1, 2))^2 - (M(i-1, n_x))^2);
+    M(i, n_x+1) = M(i, 1);
 end
-plot(x_data_1, M(38, :), 'r', linspace(-1, 1, 5001), N(38, :), 'b')
+
+x_data_2 = linspace(-1, 1, n_x+1);
+y_data_2 = M(n_t+1, :);
+plot(x_data_1, y_data_1, 'r', x_data_2, y_data_2, 'b')
+
+
