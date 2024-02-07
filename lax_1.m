c544bacd359c7833fba9e123397021eda7fab419
Aryan Arora <82714299+Aryan1612@users.noreply.github.com>
2024-02-07 16:11:12 +0530

    Update lax_1.m

--- lax_1.m
+++ lax_1.m
@@ -5,7 +5,7 @@ lambda = 0.8;
 x_data_1 = linspace(-1, 1, 201);
 x_data_2 = linspace(-1, 1, 5001);
 for i = 1:201
-    if (abs(delta_x*i - 1.01) < (1/3))
+    if (abs(2*i/200 - 51/50) < (1/3))
         M(1, i) = 1;
     end
 end
@@ -18,20 +18,22 @@ for row = 2:501
     M(row, 201) = M(row, 1);
 end
 M
-N = zeros(501, 5001);
 
-for i = 1:5001
-    if (abs((2/5000)*i -2501/2500)< (1/3))
+
+N = zeros(501, 50001);
+for i = 1:50001
+    if (abs((2/50000)*i -25001/25000)< (1/3))
         N(1, i) = 1;
     end
 end
 
 for row = 2:501
-    for col = 2:5000
+    for col = 2:50000
         N(row, col) = N(row-1, col+1)*0.1 + N(row-1, col-1)*0.9;
     end
-    N(row, 1) = N(row-1, 5000) *0.9 + N(row-1, 2)*0.1;
-    N(row, 5001) = N(row, 1);
+    N(row, 1) = N(row-1, 50000) *0.9 + N(row-1, 2)*0.1;
+    N(row, 50001) = N(row, 1);
 end
 N
-plot(x_data_1, M(501, :), 'r', x_data_2, N(501, :), 'b')
+plot(x_data_1, M(501, :), 'r', linspace(-1, 1, 50001), N(501, :), 'b')
+
