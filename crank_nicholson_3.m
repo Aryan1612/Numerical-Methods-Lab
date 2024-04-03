d808a5ac7b28181131ff9e5730aa5dca9d371d3e
Aryan Arora <82714299+Aryan1612@users.noreply.github.com>
2024-04-03 17:43:52 +0530

    Update and rename cn3.m to crank_nicholson_3.m

--- crank_nicholson_3.m
+++ crank_nicholson_3.m
@@ -1,4 +1,4 @@
-dx = 0.2;
+dx = 1/50;
 dt = 0.01;
 alpha = 1;
 lambda = alpha * (dt/(dx^2));
@@ -24,16 +24,19 @@ c = lambda;
 B = diag(a*ones(1,cols-2)) + diag(b*ones(1,cols-3),1) + diag(c*ones(1,cols-3),-1);
 
 for i = 2:rows
-    x = B*U(i-1, 2:cols-1)'\A;
+    x = inv(A)*B*U(i-1, 2:cols-1)';
     for j = 1:cols-2
         U(i, j+1) = x(j);
     end
 end
 
 
-x1 = linspace(0, 1, 1/dx+ 1);
-y1 = U(rows, :);
-plot(linspace(0, 1, 1/dx+ 1), U(rows, :),'r')
+x1 = linspace(0, 1, cols);
+y1 = linspace(0, 0.05, rows);
+z = surf(x1, y1, U);
+z.EdgeColor = "none";
+
+U(rows, :)
 
 dx = 1/50;
 lambda = 0.5;
@@ -45,7 +48,7 @@ rows = floor(0.1/dt) +1;
 W = zeros(rows, cols);
 
 for i = 1:cols
-    W(1, i) = sin((i-1)*dx);
+    W(1, i) = sin(pi*(i-1)*dx)+sin(2*pi*(i-1)*dx);
 end
 
 for i = 2:rows
@@ -54,13 +57,9 @@ for i = 2:rows
     end
 end
 
+W(rows, :)
 x2 = linspace(0, 1, cols);
-y2 = W(rows, :);
-
-plot(x1, y1, "red", x2, y2, 'blue')
-
-
-
-    
-
+y2 = linspace(0, 0.05, rows);
+s = surf(x2, y2, W);
+s.EdgeColor = 'none';
 
